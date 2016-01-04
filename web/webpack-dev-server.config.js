c551834dc64a60c5e6bac7464dd32e0d20432cc5
元彦 <yuanyan.cao@alibaba-inc.com>
2016-01-05 00:41:22 +0800

    Init commit

---
+++ web/webpack-dev-server.config.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlPlugin = require('webpack-html-plugin');
+var HasteResolverPlugin = require('haste-resolver-webpack-plugin');
+
+var IP = '0.0.0.0';
+var PORT = 3000;
+var NODE_ENV = process.env.NODE_ENV;
+var ROOT_PATH = path.resolve(__dirname, '..');
+
+var config = {
+  paths: {
+    src: path.join(ROOT_PATH, '.'),
+    index: path.join(ROOT_PATH, 'index.ios'),
+  },
+};
+
+module.exports = {
+  ip: IP,
+  port: PORT,
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      'react-native': 'react-web',
+    },
+    extensions: ['', '.js', '.jsx', '.png', '.jpg'],
+  },
+  entry: [
+    'webpack-dev-server/client?http://' + IP + ':' + PORT,
+    'webpack/hot/only-dev-server',
+    config.paths.index,
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HasteResolverPlugin({
+      platform: 'web',
+      nodeModules: ['react-web']
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development'),
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.json$/,
+      loader: 'json',
+    }, {
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: [config.paths.src],
+      exclude: [path.resolve(__dirname, "../node_modules"),]
+    }, ]
+  }
+};
